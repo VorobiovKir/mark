4b4017145367043ea497b66d18c383c9e553df93
VorobiovKir <kvorobiov89@gmail.com>
2016-03-11 17:06:48 +0200

    add sort func

--- project/notes/static/notes/js/main.js
+++ project/notes/static/notes/js/main.js
@@ -13,4 +13,57 @@ angular
             var result = new Date(input).getTime();
             return result || input;
         };
-    });
+    })
+    .filter('toArray', function() { return function(obj) {
+        if (!(obj instanceof Object)) return obj;
+        return _.map(obj, function(val, key) {
+            return Object.defineProperty(val, '$key', {__proto__: null, value: key});
+        });
+    }})
+    .filter('orderByMonthName', function() {
+        return function(items, field, reverse) {
+            var months = ['jan', 'feb', 'mar', 'apr', 'may', 'june', 'july', 'aug', 'sept', 'oct', 'nov', 'dec']
+            var filtered = [];
+            for (var k in items) {
+                filtered.push(k);
+            }
+            filtered.sort(function(a, b) {
+                return months.indexOf(a) - months.indexOf(b);
+            })
+
+            return filtered
+        };
+    })
+    .filter('orderByNum', function() {
+        return function(items, field, reverse) {
+            var filtered = [];
+            for (var k in items) {
+                filtered.push(k);
+            }
+            return filtered.sort();
+        };
+    });;
+    // .filter('orderByKeyNum', function() {
+    //     return function(items, field, reverse) {
+    //         console.log('**********');
+    //         console.log(items);
+    //         var filtered = [];
+    //         for (var k in items.mar) {
+    //             filtered.push(k);
+    //         }
+    //         console.log('---------');
+    //         console.log(filtered);
+    //         // var filtered = [];
+    //         // for (var k in items) {
+    //         //     filtered.push(k);
+    //         // }
+    //         // filtered.sort();
+    //         // if(reverse) filtered.reverse();
+    //         // var result = [];
+    //         // for (var i in filtered) {
+    //         //     result.push({i:items[i]});
+    //         // }
+    //         // console.log(result);
+    //         // return result;
+    //     };
+    // });
