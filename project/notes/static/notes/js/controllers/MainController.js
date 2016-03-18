c775d687e62aa7baa00393064772bfb2da56cad3
VorobiovKir <kvorobiov89@gmail.com>
2016-03-18 09:27:47 +0200

    fix search sys

--- project/notes/static/notes/js/controllers/MainController.js
+++ project/notes/static/notes/js/controllers/MainController.js
@@ -375,12 +375,30 @@ var MainController = function($http, $scope) {
     };
 
     this.setAutocomplete = function() {
-        var notes = new Bloodhound({
-          datumTokenizer: Bloodhound.tokenizers.whitespace,
-          queryTokenizer: Bloodhound.tokenizers.whitespace,
 
-          local: that.filters.autocompleteSourse
-        });
+        var substringMatcher = function(strs) {
+          return function findMatches(q, cb) {
+            var matches, substringRegex;
+
+            // an array that will be populated with substring matches
+            matches = [];
+
+            // regex used to determine if a string contains the substring `q`
+            substrRegex = new RegExp(q, 'i');
+
+            // iterate through the pool of strings and for any string that
+            // contains the substring `q`, add it to the `matches` array
+            $.each(strs, function(i, str) {
+              if (substrRegex.test(str)) {
+                matches.push(str);
+              }
+            });
+
+            cb(matches);
+          };
+        };
+
+        var notes = that.filters.autocompleteSourse;
 
         $('.typeahead').typeahead({
           hint: true,
@@ -389,8 +407,27 @@ var MainController = function($http, $scope) {
         },
         {
           name: 'notes',
-          source: notes
+          limit: 10,
+          source: substringMatcher(notes)
         });
+
+
+        // var notes = new Bloodhound({
+        //   datumTokenizer: Bloodhound.tokenizers.whitespace,
+        //   queryTokenizer: Bloodhound.tokenizers.whitespace,
+
+        //   local: that.filters.autocompleteSourse
+        // });
+
+        // $('.typeahead').typeahead({
+        //   hint: true,
+        //   highlight: true,
+        //   minLength: 3
+        // },
+        // {
+        //   name: 'notes',
+        //   source: notes
+        // });
     }
 
     this.preloading = function(count) {
